2198fdc2f927abcb192f4b7314c877f9e4091032
ZipS <czwetkow.serega@yandex.ru>
2021-03-05 09:58:29 +0300

    made mock-functions

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+# HASHING EXAMPLE
+#
+# data = input('Enter plaintext data: ')
+# output = sha256(data.encode('utf-8'))
+
+import sqlite3
+from hashlib import sha256
+
+
+class Pwd_handler:
+    """docstring for Pwd_handler"""
+    def __init__(self):
+        pass
+
+    def input(self):
+        pass
+
+    def check(self):
+        pass
+
+    def set(self):
+        pass
+
+
+def setup():
+    pwd_handler = Pwd_handler()
+    print("Nice to meet you here bro :)")
+
+
 def main():
-    pass
+    setup()
 
 
 if __name__ == '__main__':
