97851e7a1712dce89e16a53805099dcbab829192
ZipS <czwetkow.serega@yandex.ru>
2021-03-11 20:35:44 +0300

    Now program is a module which importing to add password feature!

--- main.py
+++ main.py
@@ -1,90 +1,76 @@
 import sqlite3
 from hashlib import sha256
 from getpass import getpass
-import os
-from sys import platform
+
+DB_FILE_NAME = "access.db"
 
 
-class Db_handler():
-    def __init__(self):
-        self.db = sqlite3.connect("inf.db")
+class Pwd_handler:
+    def __init__(self, db_file_name):
+        self.db = sqlite3.connect(db_file_name)
         self.cursor = self.db.cursor()
-
-
-class Data_handler(Db_handler):
-    def __init__(self):
-        super().__init__()
-
-    def input(self):
-        pass
-
-    def delete(self):
-        pass
-
-    def set(self):
-        pass
-
-
-class Pwd_handler(Db_handler):
-    """docstring for Pwd_handler"""
-    def __init__(self):
-        super().__init__()
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS keys (
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS pwdhash (
             pwd TEXT
             )""")
         self.db.commit()
-        self.cursor.execute("SELECT pwd FROM keys")
+        self.cursor.execute("SELECT pwd FROM pwdhash")
         if self.cursor.fetchone() is None:
             self._new()
+        self.cursor.execute(f"SELECT pwd FROM pwdhash")
+        self.hashed_pwd = self.cursor.fetchall()[0][0]
 
     def _new(self):
-        pwd = getpass("\nI detected that you have no password here, "
-                      "enter yours: ")
+        print("I detected that you have no password here.")
+        pwd = self._create()
+        hashed_pwd = self._encrypt(pwd)
+        self.cursor.execute(f"INSERT INTO pwdhash VALUES (?)", (hashed_pwd,))
+        self.db.commit()
+        print("Your password successfully recorded!\n")
+
+    def _create(self):
+        pwd = getpass("Enter new password: ")
         pwd_rpt = getpass("Confirm your new password: ")
         while not pwd == pwd_rpt:
             print("Your passwords do not match!")
             pwd = getpass("\nEnter password: ")
             pwd_rpt = getpass("Confirm your new password: ")
-
-        hashed_pwd = sha256(pwd.encode("utf-8")).hexdigest()
-        self.cursor.execute(f"INSERT INTO keys VALUES (?)", (hashed_pwd,))
-        self.db.commit()
-        print("Your password successfully recorded!")
+        return pwd
 
     def inputpwd(self):
-        pwd = getpass("\nEnter your code-word: ")
+        pwd = getpass("Enter your code-word: ")
         correct = self._check(pwd)
         while not correct:
             pwd = getpass("Wrong! Enter again: ")
             correct = self._check(pwd)
 
-    def _check(self, pwd):
-        hashed_pwd = sha256(pwd.encode("utf-8")).hexdigest()
-        self.cursor.execute(f"SELECT pwd FROM keys WHERE pwd = '{hashed_pwd}'")
+    def _check(self, word):
+        hashed_word = sha256(word.encode("utf-8")).hexdigest()
+        self.cursor.execute(f"SELECT pwd FROM pwdhash "
+                            f"WHERE pwd = '{hashed_word}'")
         if self.cursor.fetchone() is None:
             return False
         else:
             return True
 
-    def set(self):
-        pass
+    def set_new(self):
+        print("\nTo set new password, you firstly need to enter old one")
+        self.inputpwd()
+        new_pwd = self._create()
+        new_hashed_pwd = self._encrypt(new_pwd)
+        self.cursor.execute(f"UPDATE pwdhash SET pwd = '{new_hashed_pwd}'")
+        self.db.commit()
+        print("Password successfully updated!")
 
-
-def clearwin():
-    if platform == "win32":
-        os.system("cls")
-    elif platform == "linux":
-        os.system("clear")
-    else:
-        print("Ошибка: неизвестная ОС")
+    def _encrypt(self, pwd):
+        return sha256(pwd.encode("utf-8")).hexdigest()
 
 
 def main():
-    clearwin()
-    print("Nice to meet you here bro :)")
-    pwd_handler = Pwd_handler()
+    print("Nice to meet you here bro :)\n")
+    pwd_handler = Pwd_handler(DB_FILE_NAME)
     pwd_handler.inputpwd()
-    print("Access to database granted :)")
+    print("Access granted :)")
+    pwd_handler.set_new()
 
 
 if __name__ == '__main__':
